33993aa32cdd6f428f873e378344a2a2ebd79a71
Dewanshh <73938082+Dewanshh@users.noreply.github.com>
2022-05-15 14:30:26 +0530

    Rapid Ems

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:ambi/HomeScreen.dart';
+import 'package:ambi/LoinPage.dart';
+import 'package:ambi/Onboarding.dart';
+import 'package:ambi/home.dart';
+import 'package:ambi/patientdetails.dart';
+import 'package:ambi/screens/googlemap.dart';
+import 'package:ambi/sosscreen.dart';
+import 'package:ambi/splash.dart';
+import 'package:ambi/userapp.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Splash());
+  }
+}
